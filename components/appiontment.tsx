ffc0655a5f9656de0d139bd0c524549615dcc0a2
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-04-28 20:08:18 +0530

    few changes in form of appointment

--- components/appiontment.tsx
+++ components/appiontment.tsx
@@ -4,6 +4,7 @@ import * as React from "react"
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import { Toaster, toast } from "sonner";
 
 export default function Appiontment(){
     const [date, setDate] = React.useState<Date | undefined>(new Date())
@@ -14,23 +15,25 @@ export default function Appiontment(){
         <div className="text-xl font-light text-center">
         Fill out the form below to schedule your pets next grooming appointment.
         </div>
-        <div className=" w-full flex flex-col justify-center items-center">
-            <div className=" flex flex-col   justify-around w-full items-center">
-                <div className="w-2/3 md:w-[300px] flex flex-col justify-center items-center gap-2 text-sm">Name
-                    <Input className="bg-white" type="text" placeholder="name"></Input>
-                </div>
-                <div  className="w-2/3 md:w-[300px] flex flex-col justify-center items-center gap-2 text-sm">Email
-                    <Input className="bg-white" type="email" placeholder="email"></Input>
-                </div>
-            </div>
-           
-        </div>
-        <div className="text-sm font-light w-2/3 md:w-[300px] flex flex-col justify-center items-center gap-2">
+        
+        <div className="text-sm  w-2/3 md:w-[300px] flex flex-col justify-center items-center gap-2">
             Pets Name
         <Input type="text" placeholder="pets name" className="bg-white"></Input>
             </div>
+            <div className="text-sm  w-2/3 md:w-[300px] flex flex-col justify-center items-center gap-2">
+            Name
+        <Input type="text" placeholder="name" className="bg-white"></Input>
+            </div>
+            <div className="text-sm  w-2/3 md:w-[300px] flex flex-col justify-center items-center gap-2">
+            Email
+        <Input type="email" placeholder="email" className="bg-white"></Input>
+            </div>
+            <div className="text-sm  w-2/3 md:w-[300px] flex flex-col justify-center items-center gap-2">
+            Phone Number
+        <Input type="number" placeholder="phone number" className="bg-white"></Input>
+            </div>
             <div className=" flex flex-col justify-center items-center gap-2">
-        selectdate
+        Select Date
         <Calendar
       mode="single"
       selected={date}
@@ -38,10 +41,13 @@ export default function Appiontment(){
       className="rounded-md border shadow bg-white"
     />
             </div>
-            <div className=" flex flex-col justify-center items-center">
-                additional notes
+            <div className=" flex flex-col justify-center items-center gap-2">
+                Additional notes
                 <Textarea placeholder="Type your message here." className="bg-white  w-[300px]"></Textarea>
             </div>
-            <Button className="">Schedule Appointment</Button>
+            <Toaster richColors></Toaster>
+            <Button className="" onClick={()=>{
+                toast.success("Appointment schduled")
+            }}>Schedule Appointment</Button>
     </div>
 }
